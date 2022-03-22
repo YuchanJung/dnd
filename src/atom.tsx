a4566b24b75c2f0b6f91ce5a399cc10a23109812
yuchannn <yuchanbpp@gmail.com>
2022-03-22 13:54:44 +0900

    initial setup

---
+++ src/atom.tsx
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const initialAtom = atom({
+  key: "atom",
+  default: [],
+});
---
+++ src/theme.ts
@@ -0,0 +1,17 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#212122",
+  textColor: "white",
+  accentColor: "#ecf0f1",
+  cardBgColor: "#3D3D3D",
+  cardShadowColor: "#161618",
+};
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "#ecf0f1",
+  textColor: "black",
+  accentColor: "#222222",
+  cardBgColor: "white",
+  cardShadowColor: "#bdc3c7",
+};
